3c4a966038e2043dc4ffe00972f50254d28d32af
angelachan64 <angelachan64@gmail.com>
2016-04-11 00:35:09 +0000

    only the first decorator works but both functions work

---
+++ 09/work.py
@@ -0,0 +1,57 @@
+###############################################################################
+#         HOMEWORK 9                HOMEWORK 9                HOMEWORK 9      #
+#         HOMEWORK 9                HOMEWORK 9                HOMEWORK 9      #
+#         HOMEWORK 9                HOMEWORK 9                HOMEWORK 9      #
+#         HOMEWORK 9                HOMEWORK 9                HOMEWORK 9      #
+###############################################################################
+# Write a closure/decorator apparatus that will facilitate timing, and another
+# for output of function name and arguments, and use decorators to apply one or 
+# both of these to functions of your choosing. Store your work in hw09 subdir of
+# your hw repo.
+import time
+import random
+
+def timer(func):
+    def clock():
+        start = time.clock()
+        # time.sleep(random.randint(0,2))
+        func()
+        end = time.clock()
+        elapsed = end - start
+        return elapsed
+    return clock
+        
+def wuzzat(func):
+    def that(*arg):
+        return "Function Name: " + func.func_name + "\nArguments: " + ", ".join(str(e) for e in func(*arg))
+    return that
+        
+def qsort(l):
+    if(l==[]):
+        return []
+    lh = [x for x in l if x<l[0]]
+    uh = [x for x in l if x>l[0]]
+    return qsort(lh) + [l[0]] + qsort(uh)
+    
+@timer
+@wuzzat
+def test():
+    return qsort([random.randint(0,50) for x in range(1,30)])
+    
+guinea_pig = test()
+print guinea_pig
+
+#def make_bold(fn):
+#    return lambda : "<b>" + fn() + "</b>"
+#
+#def make_italic(fn):
+#    return lambda : "<i>" + fn() + "</i>"
+#
+#@make_bold
+#@make_italic
+#def hello():
+#    return "hello world"
+#
+#helloHTML = hello()
+#
+#print helloHTML
